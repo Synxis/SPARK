6b475dfbf7933d75e2671856821072e2865456fd
Matthieu Foulon <stardeath@wanadoo.fr>
2010-06-27 16:07:47 +0000

    mise à jour des macros

--- include/Rendering/DX9/SPK_DX9_DEF.h
+++ include/Rendering/DX9/SPK_DX9_DEF.h
@@ -49,13 +49,15 @@
 	inline FLOAT DWtoF(DWORD dw)	{ return *((FLOAT*)&dw); }
 
 	#ifndef SAFE_DELETE
-		#define SAFE_DELETE(p)       { if (p) { delete (p);     (p)=NULL; } }
+		//#define SAFE_DELETE(p)       { if (p) { delete (p);     (p)=NULL; } }
+		#define SAFE_DELETE(p)			{ delete (p); p = NULL;  }
 	#endif    
 	#ifndef SAFE_DELETE_ARRAY
-		#define SAFE_DELETE_ARRAY(p) { if (p) { delete[] (p);   (p)=NULL; } }
+		//#define SAFE_DELETE_ARRAY(p) { if (p) { delete[] (p);   (p)=NULL; } }
+		#define SAFE_DELETE_ARRAY(p)	{ delete [](p); p = NULL; }
 	#endif    
 	#ifndef SAFE_RELEASE
-		#define SAFE_RELEASE(p)      { if (p) { (p)->Release(); (p)=NULL; } }
+		#define SAFE_RELEASE(p)			{ if (p) { (p)->Release();  (p) = NULL; } }
 	#endif
 
 	inline void Assign( D3DXVECTOR3& Destination, const SPK::Vector3D& Source )
