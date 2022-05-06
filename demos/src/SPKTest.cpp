ca178b8bfddc328d6eca0ca05ea79baae8abac89
kingscallop <54776947+kingscallop@users.noreply.github.com>
2022-05-06 20:47:39 +0100

    Support OpenGL on macOS

--- demos/src/SPKTest.cpp
+++ demos/src/SPKTest.cpp
@@ -26,8 +26,14 @@
 #if defined(WIN32) || defined(_WIN32)
 #include <windows.h>
 #endif
+
+#if defined(__APPLE__)
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#else
 #include <GL/gl.h>
 #include <GL/glu.h>
+#endif
 
 #include <SDL.h>
 #include <SPARK.h>
